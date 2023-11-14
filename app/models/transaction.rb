2672c678523a942921bb4bbfd837232345328d0d
Benjamin Randles-Dunkley <ben@chemica.co.uk>
2023-11-14 16:58:01 +0000

    feat/T12-transaction-implementation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+class InsufficientBalanceError < StandardError; end
+
 # Transaction model
 class Transaction < ApplicationRecord
   belongs_to :sender, class_name: 'Account'
@@ -7,12 +9,46 @@ class Transaction < ApplicationRecord
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :status, presence: true
-  validates :error_message, presence: true
   validate :sender_and_receiver_are_different
 
   enum status: { pending: 0, completed: 1, failed: 2 }
 
+  def execute!
+    return unless pending?
+
+    transact
+  rescue InsufficientBalanceError
+    update!(status: :failed)
+    update!(error_message: 'Sender has insufficient funds')
+  rescue StandardError => e
+    update! status: :failed
+    update! error_message: 'Server error'
+  end
+
   private
+
+  def transact
+    Account.transaction do
+      s, r = lock_sender_and_receiver_accounts
+      raise InsufficientBalanceError, 'Insufficient funds' unless sender_balance_is_sufficient?(s.balance)
+
+      s.balance -= amount
+      s.save!
+      r.balance += amount
+      r.save!
+      update! status: :completed
+    end
+  end
+
+  def lock_sender_and_receiver_accounts
+    s = Account.lock('FOR UPDATE NOWAIT').find_by(id: sender.id)
+    r = Account.lock('FOR UPDATE NOWAIT').find_by(id: receiver.id)
+    [s, r]
+  end
+
+  def sender_balance_is_sufficient?(sender_balance)
+    sender_balance >= amount
+  end
 
   def sender_and_receiver_are_different
     return unless sender_id == receiver_id
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -20,7 +20,6 @@ RSpec.describe Transaction, type: :model do
     it { should validate_presence_of(:amount) }
     it { should validate_numericality_of(:amount).is_greater_than(0) }
     it { should validate_presence_of(:status) }
-    it { should validate_presence_of(:error_message) }
 
     it 'should validate that sender and receiver are different' do
       transaction = Transaction.new(sender: account, receiver: account, amount: 50)
@@ -31,5 +30,114 @@ RSpec.describe Transaction, type: :model do
 
   describe 'enums' do
     it { should define_enum_for(:status).with_values(pending: 0, completed: 1, failed: 2) }
+  end
+
+  describe 'transferring funds' do
+    context 'when the sender has enough funds' do
+      let(:sender) do
+        Account.create!(balance: 50, friendly_name: 'test1', user:)
+      end
+
+      let(:receiver) do
+        Account.create!(balance: 100, friendly_name: 'test2', user:)
+      end
+
+      let(:transaction) do
+        Transaction.create!(sender:, receiver:, amount: 50, status: :pending)
+      end
+
+      it 'should transfer funds from the sender to the receiver' do
+        transaction.execute!
+        sender.reload
+        receiver.reload
+        expect(sender.balance).to eq 0
+        expect(receiver.balance).to eq 150
+      end
+
+      it 'should mark the transaction as completed' do
+        transaction.execute!
+        expect(transaction.completed?).to be true
+      end
+    end
+
+    context 'when the sender does not have enough funds' do
+      let(:sender) do
+        Account.create!(balance: 10, friendly_name: 'test3', user:)
+      end
+
+      let(:receiver) do
+        Account.create!(balance: 100, friendly_name: 'test4', user:)
+      end
+
+      let(:transaction) do
+        Transaction.create!(sender:, receiver:, amount: 50, status: :pending)
+      end
+
+      it 'should not transfer funds from the sender to the receiver' do
+        transaction.execute!
+        expect(transaction.sender.balance).to eq 10
+        expect(transaction.receiver.balance).to eq 100
+      end
+
+      it 'should mark the transaction as failed' do
+        transaction.execute!
+        expect(transaction.failed?).to be true
+      end
+
+      it 'should record the failure reason' do
+        transaction.execute!
+        expect(transaction.error_message).to eq 'Sender has insufficient funds'
+      end
+
+      context 'when the transaction has failed' do
+        let(:sender) do
+          Account.create(balance: 50, friendly_name: 'test5', user:)
+        end
+
+        let(:receiver) do
+          Account.create(balance: 100, friendly_name: 'test6', user:)
+        end
+
+        let(:transaction) do
+          Transaction.create!(sender:, receiver:, amount: 50, status: :failed)
+        end
+
+        it 'should not transfer funds from the sender to the receiver' do
+          transaction.execute!
+          expect(sender.balance).to eq 50
+          expect(receiver.balance).to eq 100
+        end
+
+        it 'should not mark the transaction as completed' do
+          transaction.execute!
+          expect(transaction.failed?).to be true
+        end
+      end
+
+      context 'when the transaction has already completed' do
+        let(:sender) do
+          Account.create(balance: 50, friendly_name: 'test7', user:)
+        end
+
+        let(:receiver) do
+          Account.create(balance: 100, friendly_name: 'test8', user:)
+        end
+
+        let(:transaction) do
+          Transaction.create!(sender: user.account, receiver: account, amount: 50, status: :completed)
+        end
+
+        it 'should not transfer funds from the sender to the receiver' do
+          transaction.execute!
+          expect(sender.balance).to eq 50
+          expect(receiver.balance).to eq 100
+        end
+
+        it 'should not mark the transaction as completed' do
+          transaction.execute!
+          expect(transaction.completed?).to be true
+        end
+      end
+    end
   end
 end
